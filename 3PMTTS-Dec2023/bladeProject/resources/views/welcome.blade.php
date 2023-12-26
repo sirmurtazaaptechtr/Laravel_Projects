bfd1dd8b2d526b6c45e496a5944b88a9499a6d20
Syed Murtaza Hussain <sirmurtazaaptechtr@outlook.com>
2023-12-26 16:17:59 +0500

    26-Dec-2023-I

--- 3PMTTS-Dec2023/bladeProject/resources/views/welcome.blade.php
+++ 3PMTTS-Dec2023/bladeProject/resources/views/welcome.blade.php
@@ -1,3 +1,8 @@
+@php
+    $pagetitle = 'Zay Shop - All Customers';
+@endphp
+@includeIf('include.connection');
+@include('include.header',['title'=> $pagetitle]);
 {{ "<h1>Welcome</h1>" }}
 {!! "<h1>Welcome</h1>" !!}
 {{ "Welcome" }}
@@ -6,12 +11,13 @@
 @endphp
 <ul>
     @foreach ($students as $name=>$age)
-        @if($loop->first)
-            <li style="color: green">{{ $loop->iteration . "/" . $loop->count . " @ " . $loop->index . " - Dear $name, you are $age years old."}}</li>
-        @elseif ($loop->last)
-            <li style="color: red">{{ $loop->iteration . "/" . $loop->count . " @ " . $loop->index . " - Dear $name, you are $age years old."}}</li>
-        @else
-            <li>{{ $loop->iteration . "/" . $loop->count . " @ " . $loop->index . " - Dear $name, you are $age years old."}}</li>    
-        @endif
+    @if($loop->first)
+    <li style="color: green">{{ $loop->iteration . "/" . $loop->count . " @ " . $loop->index . " - Dear $name, you are $age years old."}}</li>
+    @elseif ($loop->last)
+    <li style="color: red">{{ $loop->iteration . "/" . $loop->count . " @ " . $loop->index . " - Dear $name, you are $age years old."}}</li>
+    @else
+    <li>{{ $loop->iteration . "/" . $loop->count . " @ " . $loop->index . " - Dear $name, you are $age years old."}}</li>    
+    @endif
     @endforeach
 </ul>
+@include('include.footer');
