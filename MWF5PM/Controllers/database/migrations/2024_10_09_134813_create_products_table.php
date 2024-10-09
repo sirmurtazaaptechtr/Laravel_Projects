58e161d625b3f09238552b5182779ca7fef60058
Syed Murtaza Hussain <sirmurtazaaptechtr@outlook.com>
2024-10-09 19:15:53 +0500

    Migration file created

---
+++ MWF5PM/Controllers/database/migrations/2024_10_09_134813_create_products_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('product_name', 30);
+            $table->string('unit', 15);
+            $table->decimal('unit_price', 8, 2); // Corrected here
+            $table->string('description', 250); // Corrected here
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
